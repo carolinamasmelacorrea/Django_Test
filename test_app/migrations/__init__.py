d792d3e90d958536f9bcd7cb05c9c34f174a0811
carolinamasmelacorrea <carolina.masmela@geophysicsgpr.com>
2023-08-11 15:46:52 -0500

    test commit

---
+++ test_app/migrations/__init__.py
@@ -0,0 +1,1 @@
+#migration guarda cambios a la base de datos
---
+++ test_app/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+#estructura base de datos, nombre, tipos de cmapos,relaciones 
