b6dfa464810d8f6e2165824e288e100c013ee1c7
GPRJulianParra <julian.parra@geophysicsgpr.com>
2023-08-11 16:05:37 -0500

    added Person model

--- test_app/models.py
+++ test_app/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here.
 #estructura base de datos, nombre, tipos de cmapos,relaciones 
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
